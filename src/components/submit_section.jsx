bdbb22ceabb1181d8c9f2141c77105501a44fdf2
Sabhya <sabhya12.saini@gmail.com>
2024-09-15 22:27:07 +0530

    Revert "[1]- 3 Forms for ugc, login redirects and userPage (#11)"
    This reverts commit 3a11529edf939437ae46ffc505929dc2e39415b5.

--- src/components/submit_section.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SubmitSection = () => (
-  <div style={{ textAlign: 'center', marginTop: '20px' }}>
-    <button type="submit">Submit Event</button>
-  </div>
-);
-
-export default SubmitSection;
